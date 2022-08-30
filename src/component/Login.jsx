dd4bdba261f178a05f05fd4394731d0fd199dfe9
Abolfazl <abolfazl30not30@gmail.com>
2022-08-30 21:50:31 +0430

    finish login form

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "../style/loginStyle.css";
+import logo from "../img/sadat logo-png.png";
 
 class Login extends Component {
   state = {};
@@ -9,7 +10,7 @@ class Login extends Component {
         <div className="container">
           <div className="left-side">
             <div className="title">
-              <h2>سعـادت پرتال</h2>
+              <h2>سعـادت پـرتـال</h2>
             </div>
             <div className="form-container">
               <form className="login-form">
@@ -35,16 +36,18 @@ class Login extends Component {
                     />
                     <label htmlFor="chk-remember">مرا به خاطر بسپار</label>
                   </div>
-                  <a href="#" className="forgot">
-                    فراموشی رمز؟
-                  </a>
+                  <div className="forgotDiv">
+                    <a href="#" className="forgot">
+                      فراموشی رمز؟
+                    </a>
+                  </div>
                 </div>
                 <button className="btn-login">ورود</button>
               </form>
             </div>
           </div>
           <div className="right-side">
-            <img src=""></img>
+            <img src={logo}></img>
           </div>
         </div>
       </>
