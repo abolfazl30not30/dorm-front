2eb1cbe3705d07daacee071dbd6b885517506834
Abolfazl <abolfazl30not30@gmail.com>
2022-09-02 22:37:39 +0430

    fix toggle button of sidebar menu

--- src/component/MainDashboard.jsx
+++ src/component/MainDashboard.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Header from './Dashboard/Header';
+import "../style/hamburgerMenu.css"
 import MainContext from '../contexts/ContextProvider';
+import HamburgerMenu from './Dashboard/HamburgerMenu';
 
 class MainDashboard extends Component {
     state = {
@@ -10,7 +12,15 @@ class MainDashboard extends Component {
         return (
             <MainContext.Provider value={{ handleSidebar: this.handleSidebar }}>
                 <Header />
-                {console.log(hello)}
+                {this.state.activeMenu ? (
+                    <div className='sidenav' style={{ animation: "open-sidebar 0.3s linear both" }}>
+                        <HamburgerMenu />
+                    </div>
+                ) : (
+                    <div className="sidenav" style={{ animation: "close-sidebar 0.3s linear both" }}>
+                        <HamburgerMenu />
+                    </div>
+                )}
             </MainContext.Provider>
 
 
@@ -19,10 +29,11 @@ class MainDashboard extends Component {
 
     handleSidebar = () => {
         if (this.state.activeMenu) {
-            this.state.activeMenu = false;
+            this.setState({ activeMenu: false })
         } else {
-            this.state.activeMenu = true;
+            this.setState({ activeMenu: true })
         }
+        console.log("hello")
     }
 }
 
