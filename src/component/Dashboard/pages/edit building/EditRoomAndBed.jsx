160fa6dc3a008059f4646a6d6392c2c75e388dab
Abolfazl <abolfazl30not30@gmail.com>
2022-09-09 21:38:51 +0430

    desing edit room and bed page

--- src/component/Dashboard/pages/edit building/EditRoomAndBed.jsx
+++ src/component/Dashboard/pages/edit building/EditRoomAndBed.jsx
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 import { AiOutlinePlus, AiFillCloseCircle } from 'react-icons/ai';
+import { FaPencilAlt } from 'react-icons/fa';
 import { EditText, EditTextarea } from 'react-edit-text';
 import { BiBed } from 'react-icons/bi';
+import CounterInput from "react-counter-input";
+import "../../../../style/editBuilding.css";
+import { Padding } from '@syncfusion/ej2-react-charts';
 
 class EditRoomAndBed extends Component {
     state = {
@@ -28,7 +32,6 @@ class EditRoomAndBed extends Component {
                 accessory: [
                     { name: "یخچال", count: "1" },
                     { name: "کمد", count: "2" },
-                    { name: "چوب لباسی", count: "1" },
                 ],
                 beds: [
                     { id: 21, bedName: 'تخت 21', empty: true },
@@ -61,24 +64,22 @@ class EditRoomAndBed extends Component {
                     </div>
                     <div className="row pb-5">
                         {this.state.rooms.map((room, i) => (
-                            <div className='col-12 p-3'>
+                            <div className='col-12'>
                                 <div className='room-box'>
-                                    <button className="room-close-btn" onClick={() => { }}><AiFillCloseCircle color="#F1416C" /></button>
+                                    <button className="close-btn" onClick={() => { this.deleteRoom(room) }}><AiFillCloseCircle color="#F1416C" /></button>
                                     <div className="title-container row">
-                                        <div className="col-6"><div className="firstTitle"><EditText showEditButton defaultValue={room.roomName} /></div></div>
-                                        <div className='col-6'><div className="description"><EditText showEditButton defaultValue={room.description} /></div></div>
+                                        <div className="col-6 d-flex align-item-center"><div className="firstTitle"><label>نام:</label><EditText showEditButton defaultValue={room.roomName} editButtonContent={<FaPencilAlt color="#f39c12" fontSize="16px" />} /></div></div>
+                                        <div className='col-6  d-flex align-item-center'><div className="description"><label>توضیحات:</label><EditText showEditButton defaultValue={room.description} editButtonContent={<FaPencilAlt color="#f39c12" fontSize="15px" />} /></div></div>
                                     </div>
-
-
                                     <div className='row'>
                                         <div className='col-6 row bed-container'>
                                             {
                                                 room.beds.map((bed) => (
                                                     <div className='col-4'>
                                                         <div className='bed-box'>
-                                                            <button className="bed-close-btn" onClick={() => { }}><AiFillCloseCircle color="#F1416C" /></button>
+                                                            <button className="close-btn" onClick={() => { this.deleteBed(bed, i) }}><AiFillCloseCircle color="#F1416C" /></button>
                                                             <BiBed fontSize="2rem" />
-                                                            <div className="title"><EditText className="editable" showEditButton defaultValue={bed.bedName} /></div>
+                                                            <div className="title"><EditText className="editable" showEditButton defaultValue={bed.bedName} editButtonContent={<FaPencilAlt color="#f39c12" fontSize="15px" />} /></div>
                                                         </div>
                                                     </div>
                                                 ))
@@ -89,16 +90,18 @@ class EditRoomAndBed extends Component {
                                         </div>
                                         <div className='col-6'>
                                             <div className='accessory-box'>
+                                                <div className="accessory-box-title"><h5>تجهیزات</h5><h5>تعداد</h5></div>
                                                 {room.accessory.map((accessory) => (
                                                     <div className="accessory row">
-                                                        <div className="accessory-title col-6">
-                                                            <h6>{accessory.name}</h6>
+                                                        <div className="accessory-title col-7">
+                                                            <EditText style={{ backgroundColor: "#f9f9f9" }} className="editable" showEditButton defaultValue={accessory.name} editButtonContent={<FaPencilAlt color="#f39c12" fontSize="15px" />} />
                                                         </div>
-                                                        <div className="accessory-count col-6">
-                                                            <h6>{accessory.count}</h6>
+                                                        <div className="accessory-count col-5">
+                                                            <CounterInput min={0} max={10} onCountChange={count => console.log(count)} />
                                                         </div>
                                                     </div>
                                                 ))}
+                                                <button onClick={() => { }} className="accessory-add-btn"><AiOutlinePlus /></button>
                                             </div>
                                         </div>
                                     </div>
@@ -106,9 +109,70 @@ class EditRoomAndBed extends Component {
                             </div>
                         ))}
                     </div>
+                    <div className="register">
+                        <button className="register-btn-room">ثـبـت</button>
+                    </div>
                 </div>
             </>
         );
+    }
+    addRoom = () => {
+        // const newFloor = this.state.floor.concat(
+        //     {
+        //         id: 3, floorName: "طبقه ....",
+        //         unit: []
+        //     }
+        // )
+        // this.setState({ floor: newFloor })
+    }
+
+    addBed = (f) => {
+        // const index = this.state.floor.indexOf(f);
+        // const newUnit = this.state.floor[index].unit.concat(
+        //     { id: 311, unitName: "new" }
+        // )
+        // const updateFloor = [...this.state.floor]
+        // updateFloor[index].unit = newUnit
+        // this.setState({ floor: updateFloor })
+    }
+
+    editRoomTitle = ({ value, previousValue }) => {
+        // const index = this.state.floor.findIndex(({ floorName }) => floorName === previousValue);
+        // const updatedState = [...this.state.floor];
+        // updatedState[index].floorName = value;
+        // this.setState({ floor: updatedState });
+    };
+
+    editBedTitle = ({ value, previousValue }) => {
+        // let indexOfFloor = -1;
+        // let indexOfUnit = -1;
+
+        // for (let x = 0; x < this.state.floor.length; x++) {
+        //     indexOfUnit = this.state.floor[x].unit.findIndex(({ unitName }) => unitName === previousValue);
+        //     if (indexOfUnit !== -1) {
+        //         indexOfFloor = x;
+        //         break;
+        //     }
+        // }
+        // const updatedState = [...this.state.floor];
+        // updatedState[indexOfFloor].unit[indexOfUnit].unitName = value;
+        // this.setState({ floor: updatedState });
+        // console.log(this.state.floor)
+    }
+
+    deleteRoom = (room) => {
+        const updateState = this.state.rooms.filter(r => r !== room);
+        this.setState({ rooms: updateState });
+        console.log(this.state.rooms)
+    }
+
+    deleteBed = (bed, index) => {
+        let updatedState = [...this.state.rooms];
+        let updatedBed = this.state.rooms[index].beds;
+        updatedBed = updatedBed.filter(b => b !== bed);
+        updatedState[index].beds = updatedBed;
+        this.setState({ rooms: updatedState });
+        console.log(this.state.rooms)
     }
 }
 
