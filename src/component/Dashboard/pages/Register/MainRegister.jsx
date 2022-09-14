bd505d66b8ef48aef9c3898231231beec1b4566e
Abolfazl <abolfazl30not30@gmail.com>
2022-09-14 16:22:28 +0430

    add family guest and other guest page

--- src/component/Dashboard/pages/Register/MainRegister.jsx
+++ src/component/Dashboard/pages/Register/MainRegister.jsx
@@ -12,26 +12,84 @@ import OtherGuest from './OtherGuest'
 
 class MainRegister extends Component {
     state = {
-        typeofResident: "Constant"
+        typeofResident: "Constant",
+        steps: [
+            {
+                label: 'نوع اقامتگر',
+                name: 'step 1',
+                content:
+                    <div className='typeofResident'>
+                        <div class="">
+                            <input className="" type="radio" name="flexRadioDefault" value='constant' onChange={(e) => { this.checked(e) }} />
+                            <label class="" for="Radio1">
+                                اقامتگر ثابت
+                            </label>
+                        </div>
+                        <h6>مهمان</h6>
+                        <div>
+                            <div class="">
+                                <input className="" type="radio" name="flexRadioDefault" value='otherGuest' onChange={(e) => { this.checked(e) }} />
+                                <label class="" for="Radio2">
+                                    متفرقه
+                                </label>
+                            </div>
+                            <div class="">
+                                <input className="" type="radio" name="flexRadioDefault" value='familyGuest' onChange={(e) => { this.checked(e) }} />
+                                <label class="" for="Radio3">
+                                    بستگان درجه یک
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+            },
+            {
+                label: 'مشخصات اولیه',
+                name: 'step 1',
+                content: ""
+            },
+            {
+                label: 'مشخصات تکمیلی',
+                name: 'step 2',
+                content: <InformationFurtherPage />
+
+            },
+            {
+                label: 'مشخصات بستگان',
+                name: 'step 3',
+                content: <InformationFamilyPage />
+            },
+            {
+                label: 'آپلود مدارک',
+                name: 'step 4',
+                content: <UploadPage />
+            }
+        ]
     }
+
     checked = (e) => {
         const type = e.target.value
-        this.setState({typeofResident:type})
-
+        this.setState({ typeofResident: type })
 
         switch (type) {
-            case 'Constant':
-                return <InformationPage />;
+            case 'constant': {
+                let updatedState = [...this.state.steps];
+                updatedState[1].content = <InformationPage />;
+                this.setState({ steps: updatedState })
                 break;
-            case 'otherGuest':
-                return <FamilyGuest />;
+            }
+            case 'otherGuest': {
+                let updatedState = [...this.state.steps];
+                updatedState[1].content = <FamilyGuest />;
+                this.setState({ steps: updatedState })
                 break;
-            case 'otherGuest':
-                return <OtherGuest />;
-                break;        
+            }
+            case 'otherGuest': {
+                let updatedState = [...this.state.steps];
+                updatedState[1].content = <OtherGuest />;;
+                this.setState({ steps: updatedState })
+                break;
+            }
         }
-
-
     }
     render() {
         return (
@@ -44,72 +102,7 @@ class MainRegister extends Component {
                         submitBtnName={"ارسال"}
                         primaryBtnClass="next-btn"
                         secondaryBtnClass="previous-btn"
-                        steps={[
-                            {
-                                label: 'نوع اقامتگر',
-                                name: 'step 1',
-                                content:
-                                    <div className='typeofResident'>
-                                        <div class="">
-                                            <input className="" type="radio" name="flexRadioDefault" value='Constant'  onChange={this.checked} />
-                                            <label class="" for="Radio1">
-                                                اقامتگر ثابت
-                                            </label>
-                                        </div>
-                                        <h6>مهمان</h6>
-                                        <div>
-                                            <div class="">
-                                                <input className="" type="radio" name="flexRadioDefault" value='otherGuest' onChange={this.checked}/>
-                                                <label class="" for="Radio2">
-                                                    متفرقه
-                                                </label>
-                                            </div>
-                                            <div class="">
-                                                <input className="" type="radio" name="flexRadioDefault" value='familyGuest' onChange={this.checked}/>
-                                                <label class="" for="Radio3">
-                                                    بستگان درجه یک
-                                                </label>
-                                            </div>
-                                        </div>
-                                    </div>
-                            },
-                            {
-                                label: 'مشخصات اولیه',
-                                name: 'step 1',
-                                content:  <FamilyGuest /> 
-                                // content: <>
-                                //     {
-                                //         // console.log(this.state.typeofResident === 'Constant')
-                                //         // this.state.typeofResident === 'Constant' ? <InformationPage /> : <FamilyGuest />
-                                //         // switch (this.state.typeofResident) {
-                                //         //     case 'Constant':
-                                //         //         return <InformationPage />;
-                                //         //         break;
-                                //         //     case 'otherGuest':
-                                //         //         return <InformationPage />;
-                                //         //         break;    
-                                //         // }
-                                        
-                                //     }
-                                // </>
-                            },
-                            {
-                                label: 'مشخصات تکمیلی',
-                                name: 'step 2',
-                                content: <InformationFurtherPage />
-
-                            },
-                            {
-                                label: 'مشخصات بستگان',
-                                name: 'step 3',
-                                content: <InformationFamilyPage />
-                            },
-                            {
-                                label: 'آپلود مدارک',
-                                name: 'step 4',
-                                content: <UploadPage />
-                            }
-                        ]}
+                        steps={this.state.steps}
                     />
                 </div>
             </>
