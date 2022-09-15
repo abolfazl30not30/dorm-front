73cc8b962fd2914d71aee7f4664b4a7614df1f9c
Abolfazl <abolfazl30not30@gmail.com>
2022-09-15 20:56:33 +0430

    add edit floor api

--- src/component/Dashboard/pages/Inventory and capacity/RoomAndBed.jsx
+++ src/component/Dashboard/pages/Inventory and capacity/RoomAndBed.jsx
@@ -1,17 +1,17 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './../../../../style/roomAndBed.css'
-import {BiBed} from 'react-icons/bi'
-import {TbBuilding} from 'react-icons/tb'
-import {MdDateRange} from 'react-icons/md'
-import {AiOutlineNumber} from 'react-icons/ai'
-import {AiOutlineUser} from 'react-icons/ai'
-import {AiOutlineMore} from 'react-icons/ai'
-import {Link, Route, Routes} from "react-router-dom";
+import { BiBed } from 'react-icons/bi'
+import { TbBuilding } from 'react-icons/tb'
+import { MdDateRange } from 'react-icons/md'
+import { AiOutlineNumber } from 'react-icons/ai'
+import { AiOutlineUser } from 'react-icons/ai'
+import { AiOutlineMore } from 'react-icons/ai'
+import { Link, Route, Routes } from "react-router-dom";
 import FullViewOfBed from './FullViewOfBed';
 import BuildingContext from '../../../../contexts/Building';
 import FloorAndBedLoading from '../../../loading/FloorAndBedLoading';
-import {Button, Modal} from 'react-bootstrap'
-import {BiChevronLeft} from 'react-icons/bi'
+import { Button, Modal } from 'react-bootstrap'
+import { BiChevronLeft } from 'react-icons/bi'
 
 class RoomAndBed extends Component {
     static contextType = BuildingContext;
@@ -20,7 +20,7 @@ class RoomAndBed extends Component {
             {
                 id: 1, roomName: 'اتاق 1',
                 bed: [
-                    {id: 11, bedName: 'تخت 11', empty: true},
+                    { id: 11, bedName: 'تخت 11', empty: true },
                     {
                         id: 12, bedName: 'تخت 12', empty: false,
                         person: {
@@ -33,16 +33,16 @@ class RoomAndBed extends Component {
                             image: 'https://docs.microsoft.com/answers/storage/attachments/209536-360-f-364211147-1qglvxv1tcq0ohz3fawufrtonzz8nq3e.jpg'
                         }
                     },
-                    {id: 13, bedName: 'تخت 13', empty: true},
-                    {id: 14, bedName: 'تخت 14', empty: true},
-                    {id: 15, bedName: 'تخت 15', empty: true},
+                    { id: 13, bedName: 'تخت 13', empty: true },
+                    { id: 14, bedName: 'تخت 14', empty: true },
+                    { id: 15, bedName: 'تخت 15', empty: true },
 
                 ]
             },
             {
                 id: 2, roomName: 'اتاق 2',
                 bed: [
-                    {id: 21, bedName: 'تخت 21', empty: true},
+                    { id: 21, bedName: 'تخت 21', empty: true },
                     {
                         id: 22, bedName: 'تخت 22', empty: false,
                         person: {
@@ -55,16 +55,16 @@ class RoomAndBed extends Component {
                             image: 'https://www.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png'
                         }
                     },
-                    {id: 23, bedName: 'تخت 23', empty: true},
-                    {id: 24, bedName: '24 تخت', empty: true},
-                    {id: 25, bedName: 'تخت 25', empty: true},
+                    { id: 23, bedName: 'تخت 23', empty: true },
+                    { id: 24, bedName: '24 تخت', empty: true },
+                    { id: 25, bedName: 'تخت 25', empty: true },
 
                 ]
             },
             {
                 id: 3, roomName: 'اتاق 3',
                 bed: [
-                    {id: 31, bedName: '31 تخت', empty: true},
+                    { id: 31, bedName: '31 تخت', empty: true },
                     {
                         id: 32, bedName: 'تخت 32', empty: false,
                         person: {
@@ -77,105 +77,111 @@ class RoomAndBed extends Component {
                             image: 'https://www.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png'
                         }
                     },
-                    {id: 33, bedName: 'تخت 33', empty: true},
-                    {id: 34, bedName: 'تخت 34', empty: true},
-                    {id: 35, bedName: 'تخت 35', empty: true},
+                    { id: 33, bedName: 'تخت 33', empty: true },
+                    { id: 34, bedName: 'تخت 34', empty: true },
+                    { id: 35, bedName: 'تخت 35', empty: true },
 
                 ]
             },
             {
                 id: 4, roomName: 'اتاق 4',
                 bed: [
-                    {id: 41, bedName: '41 تخت', empty: true},
-                    {id: 42, bedName: '42 تخت', empty: true},
-                    {id: 43, bedName: 'تخت 43', empty: true},
-                    {id: 44, bedName: 'تخت 44', empty: true},
-                    {id: 45, bedName: 'تخت 45', empty: true},
+                    { id: 41, bedName: '41 تخت', empty: true },
+                    { id: 42, bedName: '42 تخت', empty: true },
+                    { id: 43, bedName: 'تخت 43', empty: true },
+                    { id: 44, bedName: 'تخت 44', empty: true },
+                    { id: 45, bedName: 'تخت 45', empty: true },
 
                 ]
             },
             {
                 id: 4, roomName: 'اتاق 5',
                 bed: [
-                    {id: 51, bedName: '51 تخت', empty: true},
-                    {id: 52, bedName: '52 تخت', empty: true},
-                    {id: 53, bedName: 'تخت 53', empty: true},
-                    {id: 54, bedName: '54 تخت', empty: true},
-                    {id: 55, bedName: 'تخت 55', empty: true},
+                    { id: 51, bedName: '51 تخت', empty: true },
+                    { id: 52, bedName: '52 تخت', empty: true },
+                    { id: 53, bedName: 'تخت 53', empty: true },
+                    { id: 54, bedName: '54 تخت', empty: true },
+                    { id: 55, bedName: 'تخت 55', empty: true },
 
                 ],
                 accessory: [
-                    {id: 51, accName: 'وسایل سرمایشی', count: 2},
-                    {id: 51, accName: "یخچال", count: 1},
-                    {id: 51, accName: "چوب لباسی", count: 10}
+                    { id: 51, accName: 'وسایل سرمایشی', count: 2 },
+                    { id: 51, accName: "یخچال", count: 1 },
+                    { id: 51, accName: "چوب لباسی", count: 10 }
                 ]
             },
             {
                 id: 6, roomName: 'اتاق 6',
                 bed: [
-                    {id: 61, bedName: '61 تخت', empty: true},
-                    {id: 62, bedName: '62 تخت', empty: true},
-                    {id: 63, bedName: '63 تخت', empty: true},
-                    {id: 64, bedName: 'تخت 64', empty: true},
-                    {id: 65, bedName: 'تخت 65', empty: true},
+                    { id: 61, bedName: '61 تخت', empty: true },
+                    { id: 62, bedName: '62 تخت', empty: true },
+                    { id: 63, bedName: '63 تخت', empty: true },
+                    { id: 64, bedName: 'تخت 64', empty: true },
+                    { id: 65, bedName: 'تخت 65', empty: true },
 
                 ]
             }
         ],
         isLoading: false,
+
         rooms: [],
         show: false,
         showAccessory: false,
+
         unit: {
             accessories: [
-                {name: 'یخچال', count: 1},
-                {name: 'چوب لباسی', count: 4}
+                { name: 'یخچال', count: 1 },
+                { name: 'چوب لباسی', count: 4 }
             ]
         },
+
         bedOpen:
-            {
-                id: 32, bedName: 'تخت 32', empty: false,
-                person: {
-                    id: 1,
-                    firstName: 'میلاد',
-                    lastName: 'زارع',
-                    nationalCode: 2500255252,
-                    StartOfStay: '12/34/56',
-                    age: 21,
-                    image: 'https://www.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png'
-                }
-            },
+        {
+            id: 32, bedName: 'تخت 32', empty: false,
+            person: {
+                id: 1,
+                firstName: 'میلاد',
+                lastName: 'زارع',
+                nationalCode: 2500255252,
+                StartOfStay: '12/34/56',
+                age: 21,
+                image: 'https://www.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png'
+            }
+        },
         roomAccessory: {
             accessories: [
-                {name: 'یخچال', count: 1},
-                {name: 'چوب لباسی', count: 4}
+                { name: 'یخچال', count: 1 },
+                { name: 'چوب لباسی', count: 4 }
             ]
         }
     }
 
     async componentDidMount() {
         const response = await fetch(`http://api.saadatportal.com/api/v1/unit/room/${this.context.unitId}`).then((response) => response.json())
-            .then((data) => this.setState({rooms: data, isLoading: false}));
+            .then((data) => this.setState({ rooms: data, isLoading: false }));
         const responseUnit = await fetch(`http://api.saadatportal.com/api/v1/unit/${this.context.unitId}`).then((response) => response.json())
-            .then((data) => this.setState({unit: data, isLoading: false}));
+            .then((data) => this.setState({ unit: data, isLoading: false }));
+
+        console.log(this.state.rooms);
+        console.log(this.state.unit);
     }
 
     handleClose = () => {
-        this.setState({show: false})
+        this.setState({ show: false })
     };
 
     handleShow = (bed, room) => {
-        this.setState({roomAccessory: room})
-        this.setState({bedOpen: bed})
-        this.setState({show: true})
+        this.setState({ roomAccessory: room })
+        this.setState({ bedOpen: bed })
+        this.setState({ show: true })
     };
 
     handleCloseAccessory = () => {
-        this.setState({showAccessory: false})
+        this.setState({ showAccessory: false })
     };
 
     handleShowAccessory = () => {
-        this.setState({showAccessory: true})
+        this.setState({ showAccessory: true })
     };
 
     render() {
@@ -197,14 +203,14 @@ class RoomAndBed extends Component {
                     <h2 className='unit-name'>
                         <button className='btn' onClick={() => {
                             this.handleShowAccessory(this.context.unitNumber)
-                        }}><AiOutlineMore className="mt-2" color='#555' fontSize="1.8rem"/></button>
-                        <TbBuilding className="mt-2" color='#555' fontSize="1.8rem"/>
+                        }}><AiOutlineMore className="mt-2" color='#555' fontSize="1.8rem" /></button>
+                        <TbBuilding className="mt-2" color='#555' fontSize="1.8rem" />
                         <span className="unit-title">واحد {this.context.unitNumber}</span>
                     </h2>
                     {
                         this.state.isLoading ? (
-                            <div className='row' style={{marginTop: "60px"}}>
-                                <FloorAndBedLoading/>
+                            <div className='row' style={{ marginTop: "60px" }}>
+                                <FloorAndBedLoading />
                             </div>
                         ) : (
                             <div className="d-flex flex-wrap">
@@ -222,7 +228,7 @@ class RoomAndBed extends Component {
                                                                     <Button onClick={() => {
                                                                         this.handleShow(bed, room)
                                                                     }}>
-                                                                        <BiBed fontSize="2rem"/>
+                                                                        <BiBed fontSize="2rem" />
                                                                         <div className="title">{bed.name}</div>
                                                                     </Button>
                                                                 </div>
@@ -245,13 +251,12 @@ class RoomAndBed extends Component {
                         <Modal.Title>{this.state.bedOpen.bedName}</Modal.Title>
                     </Modal.Header>
                     <Modal.Body>
-
                         <div className="accessory-box">
                             <div className='text-center mb-3 fw-bold'>لوازم جانبی</div>
                             <div className="d-flex flex-column">
                                 {this.state.roomAccessory.accessories.map((acc) => (
                                     <div className='d-flex flex-row my-2 w-50'>
-                                        <div className='ms-3'><BiChevronLeft/>{acc.name}</div>
+                                        <div className='ms-3'><BiChevronLeft />{acc.name}</div>
                                         <div className='me-auto'>{acc.count} عدد</div>
                                     </div>
                                 ))}
@@ -261,15 +266,15 @@ class RoomAndBed extends Component {
                             (!this.state.bedOpen.empty) ? (
                                 <div className="d-flex justify-content-center">
                                     <div className='profile-box'>
-                                        <img src={this.state.bedOpen.person.image} width='200' alt=""/>
+                                        <img src={this.state.bedOpen.person.image} width='200' alt="" />
                                         <div
                                             className="name">{this.state.bedOpen.person.firstName} {this.state.bedOpen.person.lastName}</div>
-                                        <div className='profile-item'><AiOutlineNumber className='ms-2'/>کد
+                                        <div className='profile-item'><AiOutlineNumber className='ms-2' />کد
                                             ملی: {this.state.bedOpen.person.nationalCode}</div>
-                                        <div className='profile-item'><MdDateRange className='ms-2'/>شروع
+                                        <div className='profile-item'><MdDateRange className='ms-2' />شروع
                                             اقامت: {this.state.bedOpen.person.StartOfStay}</div>
                                         <div className='profile-item'><AiOutlineUser
-                                            className='ms-2'/>سن: {this.state.bedOpen.person.age}</div>
+                                            className='ms-2' />سن: {this.state.bedOpen.person.age}</div>
                                     </div>
                                 </div>
                             ) : (
@@ -290,7 +295,7 @@ class RoomAndBed extends Component {
                         {
                             this.state.unit.accessories.map((acc) => (
                                 <div className='d-flex flex-row my-2 w-50'>
-                                    <div className='ms-3'><BiChevronLeft/>{acc.name}</div>
+                                    <div className='ms-3'><BiChevronLeft />{acc.name}</div>
                                     <div className='me-auto'>{acc.count} عدد</div>
                                 </div>
                             ))
@@ -300,8 +305,6 @@ class RoomAndBed extends Component {
                         )}
                     </Modal.Body>
                 </Modal>
-
-
             </>
         );
     }
--- src/component/Dashboard/pages/edit building/EditFloorAndUnit.jsx
+++ src/component/Dashboard/pages/edit building/EditFloorAndUnit.jsx
@@ -15,9 +15,8 @@ class EditFloorAndUnit extends Component {
     }
 
     async componentDidMount() {
-        const response = await fetch('').then((response) => response.json())
+        const response = await fetch('http://api.saadatportal.com/api/v1/floor').then((response) => response.json())
             .then((data) => this.setState({ floor: data, isLoading: false }));
-
     }
 
 
@@ -43,14 +42,14 @@ class EditFloorAndUnit extends Component {
                             <div className="col-4 p-3">
                                 <div className="floor-box">
                                     <button className="floor-close-btn" onClick={() => { this.deleteFloor(f) }}><AiFillCloseCircle color="#F1416C" /></button>
-                                    <div className="title"><EditText ref="floorTitle" showEditButton defaultValue={f.floorName} onSave={this.editFloorTitle} /></div>
+                                    <div className="title"><EditText ref="floorTitle" showEditButton defaultValue={f.name} onSave={this.editFloorTitle} /></div>
                                     <div className="d-flex flex-wrap">
-                                        {f.unit.map((u) => (
+                                        {f.units.map((u) => (
                                             <div className="col-4">
                                                 <div className="unit-box">
                                                     <button className="unit-close-btn" onClick={() => { this.deleteUnit(u, i) }}><AiFillCloseCircle color="#F1416C" /></button>
                                                     <TbBuilding fontSize="2rem" />
-                                                    <div className="title"><EditText className="editable" showEditButton onSave={this.editUnitTitle} defaultValue={u.unitName} /></div>
+                                                    <div className="title"><EditText className="editable" showEditButton onSave={this.editUnitTitle} defaultValue={u.number} /></div>
                                                 </div>
                                             </div>
                                         ))}
@@ -74,29 +73,60 @@ class EditFloorAndUnit extends Component {
     }
 
     addFloor = () => {
+        var content;
+        (async () => {
+            const rawResponse = await fetch('http://api.saadatportal.com/api/v1/floor', {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ name: "طبقه", empty: "true" })
+            });
+            content = await rawResponse.json();
+            console.log(content);
+        })();
+
         const newFloor = this.state.floor.concat(
             {
-                id: 3, floorName: "طبقه ....",
-                unit: []
+                empty: true,
+                name: "طبقه ....",
+                units: []
             }
         )
         this.setState({ floor: newFloor })
+        console.log(this.state.floor)
     }
 
     addUnit = (f) => {
         const index = this.state.floor.indexOf(f);
-        const newUnit = this.state.floor[index].unit.concat(
-            { id: 311, unitName: "new" }
+        const newUnit = this.state.floor[index].units.concat(
+            { number: "new" }
         )
         const updateFloor = [...this.state.floor]
-        updateFloor[index].unit = newUnit
+        updateFloor[index].units = newUnit
         this.setState({ floor: updateFloor })
     }
 
     editFloorTitle = ({ value, previousValue }) => {
-        const index = this.state.floor.findIndex(({ floorName }) => floorName === previousValue);
+        const floor = this.state.floor.find(({ name }) => name === previousValue);
+        const index = this.state.floor.findIndex(({ name }) => name === previousValue);
+
+        (async () => {
+            const rawResponse = await fetch(`http://api.saadatportal.com/api/v1/floor/${floor.id}`, {
+                method: 'PATCH',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ name: value, empty: "true" })
+            });
+            const content = await rawResponse.json();
+            console.log(content);
+        })();
+
         const updatedState = [...this.state.floor];
-        updatedState[index].floorName = value;
+        updatedState[index].name = value;
         this.setState({ floor: updatedState });
     };
 
@@ -105,19 +135,27 @@ class EditFloorAndUnit extends Component {
         let indexOfUnit = -1;
 
         for (let x = 0; x < this.state.floor.length; x++) {
-            indexOfUnit = this.state.floor[x].unit.findIndex(({ unitName }) => unitName === previousValue);
+            indexOfUnit = this.state.floor[x].units.findIndex(({ number }) => number === previousValue);
             if (indexOfUnit !== -1) {
                 indexOfFloor = x;
                 break;
             }
         }
         const updatedState = [...this.state.floor];
-        updatedState[indexOfFloor].unit[indexOfUnit].unitName = value;
+        updatedState[indexOfFloor].units[indexOfUnit].number = value;
         this.setState({ floor: updatedState });
         console.log(this.state.floor)
     }
 
     deleteFloor = (floor) => {
+        (async () => {
+            await fetch(`http://api.saadatportal.com/api/v1/floor/${floor.id}`, {
+                method: 'DELETE',
+            })
+                .then(res => res.text()) // or res.json()
+                .then(res => console.log(res))
+        })();
+
         const updateUsers = this.state.floor.filter(f => f !== floor);
         this.setState({ floor: updateUsers });
         console.log(this.state.floor)
@@ -125,9 +163,9 @@ class EditFloorAndUnit extends Component {
 
     deleteUnit = (unit, index) => {
         let updatedState = [...this.state.floor];
-        let updatedUnit = this.state.floor[index].unit;
+        let updatedUnit = this.state.floor[index].units;
         updatedUnit = updatedUnit.filter(u => u !== unit);
-        updatedState[index].unit = updatedUnit;
+        updatedState[index].units = updatedUnit;
         this.setState({ floor: updatedState });
         console.log(this.state.floor)
     }
