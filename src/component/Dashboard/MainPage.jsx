eb3b3045eb98da9e39683df1b0a1606a3bb3627f
Abolfazl <abolfazl30not30@gmail.com>
2022-09-15 15:11:11 +0430

    fix error

--- src/component/Dashboard/MainPage.jsx
+++ src/component/Dashboard/MainPage.jsx
@@ -7,11 +7,9 @@ import RoomAndBed from './pages/Inventory and capacity/RoomAndBed';
 import BuildingContext from '../../contexts/Building';
 import FullViewOfBed from './pages/Inventory and capacity/FullViewOfBed';
 import EditFloorAndUnit from './pages/edit building/EditFloorAndUnit';
-<<<<<<< HEAD
 import EditRoomAndBed from './pages/edit building/EditRoomAndBed';
-=======
 import MainRegister from './pages/Register/MainRegister';
->>>>>>> register_page
+
 class MainPage extends Component {
     state = {
         unitNumber: "",
@@ -24,16 +22,10 @@ class MainPage extends Component {
                     <BuildingContext.Provider value={{ unitId: this.state.unitId, unitNumber: this.state.unitNumber, handleUnitNumber: this.handleUnitNumber }}>
                         <Routes>
                             <Route path="/" element={(<Home />)} />
-                            <Route path="/booking" element={(<FloorAndUnit />)} />
                             <Route path="/booking/edit-floor-and-unit" element={(<EditFloorAndUnit />)} />
-                            <Route path="/RoomAndBed" element={(<RoomAndBed />)} />
-<<<<<<< HEAD
-                            <Route path="/" element={(<Home />)} />
                             <Route path='/editRoomAndBed' element={(<EditRoomAndBed />)} />
-=======
                             <Route path="/Register" element={(<MainRegister />)} />
                             <Route path="/edit" element={(<EditFloorAndUnit />)} />
->>>>>>> register_page
                             <Route path="/booking" element={(<FloorAndUnit />)} />
                             <Route path="/RoomAndBed" element={(<RoomAndBed />)} />
                             <Route path="/FullViewOfBed" element={(<FullViewOfBed />)} />
